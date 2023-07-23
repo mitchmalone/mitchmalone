afcf702fd53be960a494677153579d20d91425d7
Mitch Malone <mitch@itinerant.digital>
2023-07-23 06:35:17 +0200

    Adds basic generation functionality

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let DATA = {
+  name: 'Mitch',
+};
+
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+generateReadMe();
