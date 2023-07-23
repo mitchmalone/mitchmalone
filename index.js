923383655ddcb913dc4e7aaa0a122fd9bb15443d
Mitch Malone <mitch@itinerant.digital>
2023-07-23 06:58:58 +0200

    Test test 1…2..3… 🎙️

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-// index.js
 const Mustache = require('mustache');
+const fetch = require('node-fetch');
 const fs = require('fs');
+const puppeteerService = require('./services/puppeteer.service');
+
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
 let DATA = {
   name: 'Mitch',
+  now: 'Brno, Czechia',
+  refresh_date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Europe/Stockholm',
+  }),
 };
 
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+async function setInstagramPosts() {
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('mitchmalone', 3);
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
-generateReadMe();
+
+async function action() {
+  await setInstagramPosts();
+  await generateReadMe();
+  await puppeteerService.close();
+}
+
+action();
